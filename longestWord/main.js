7e42b86a3a7b33952e0e13621f6bff399ceb8124
Williams Omayuku <womayuku@gmail.com>
2016-10-24 11:45:10 +0200

    Functional code of longest word finder

---
+++ longestWord/main.js
@@ -0,0 +1,50 @@
+// File to return the longest word in a sentence
+window.addEventListener('load', function() {
+window.alert('Welcome');
+
+var form1 = document.getElementById("form");
+var text = document.getElementById('text');
+
+form1.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  var msg = '';
+  var mySentence = text.value;
+
+  //Quit if text is empty
+  if (mySentence.length == 0) {
+    msg = '<p>Please enter a sentence</p>';
+    document.getElementById('message').innerHTML = msg;
+    return;
+  }
+
+  var words = mySentence.split(' ');
+  var wordLength = words.map(function(word) {
+    return word.length;
+  })
+  wordLength.sort(function(a, b) {
+    return b - a;
+  });
+  var longestCount = wordLength[0];
+  //search the words array for the longest words
+  var longestWords = [];
+  for (var i=0; i<words.length; i++) {
+    if (longestCount == words[i].length) {
+      longestWords.push(words[i]);
+    }
+  }
+  //Output result to the browser
+  if (longestWords.length === 1) {
+    msg = '<p>The longest word in the sentence is: ' + longestWords[0] + ' with ' + longestWords[0].length + ' characters.</p>';
+  }
+  else if (longestWords.length >= 1) {
+    msg = '<p>The longest words in the sentence are: \n<ol>';
+    for (var i=0; i<longestWords.length; i++) {
+      msg +='<li>' + '   ' + longestWords[i] + ' with ' + longestWords[i].length + ' characters</li>\n';
+    }
+    msg += '</ol></p>';
+  }
+
+  document.getElementById('message').innerHTML = msg;
+});
+});
