be32ee8f673e19921000482b2b2c9e746edc6869
Williams Omayuku <womayuku@gmail.com>
2016-11-08 19:08:19 +0100

    API calls now working, quotes and author now display on screen, no tweet function yet

--- quoteMachine/main.js
+++ quoteMachine/main.js
@@ -1,5 +1,18 @@
+
+
 $(function(){
-	$("#cl").click(function(){
-		$("#cl").hide(2000);
-	});
+	$("#getQuote").click(function(e) {
+		e.preventDefault();
+	//	proc(data);
+		$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(b) {
+			console.log(b[0].content);
+			var data = b[0]
+			$("#quote").html(data.content);
+			$("#author").text("- " + data.title);
+			var a = JSON.stringify(b);
+			console.log(b);
+			alert(a);
+	
+    });
+  });
 });
